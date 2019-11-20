264b4961a31a2bd9eece88a0c919dd16ad6f1537
clemea <clematidea2@gmail.com>
2019-11-20 17:46:56 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.5c754ccb3008b14d42e94b4f2e6ff28f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bad-weather/robots.txt"
+  },
+  {
+    "revision": "c7d3b99649e3bfadbdba",
+    "url": "/bad-weather/js/chunk-vendors.bd95f912.js"
+  },
+  {
+    "revision": "ce63205fdf82af77e97a",
+    "url": "/bad-weather/js/app.4ee47c26.js"
+  },
+  {
+    "revision": "b3b6a588c3370c3121eea5065ee9eeb0",
+    "url": "/bad-weather/index.html"
+  },
+  {
+    "revision": "a5c4d371a696ef9010e3093880dea960",
+    "url": "/bad-weather/img/bg.a5c4d371.jpg"
+  },
+  {
+    "revision": "c7d3b99649e3bfadbdba",
+    "url": "/bad-weather/css/chunk-vendors.bc696e2e.css"
+  },
+  {
+    "revision": "ce63205fdf82af77e97a",
+    "url": "/bad-weather/css/app.3da8e8fe.css"
+  }
+];
